b851f0166abc717263f20a8e84a2cdd8be20f860
Ry Lickliter <rylickliter@gmail.com>
2021-11-19 16:21:42 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 :START
-DayZServer_x64.exe -cpuCount=4 -mod=@GameLabs;@CF;@CommunityOnlineTools;@DabsFramework;@Chiemsee;@SkyZ;@DayZEditorLoader;@AdditionalMedicSupplies;@TeddysWeaponPack;@RUSFormavehicles;@StreetLighting;@Notifications;@Notes;@MunghardsItempack;@BaseBuildingPlus;@BuildAnywherev3;@MuchStuffPack;@BuilderItems;@CannabisPlus;@CarCover;@CodeLock;@EarPlugs;@InventoryInCar;@ZeroyFishingZ;@zSpawnSelection;@RaGHummerArmored;@MuchCarKey;@PVEZ;@SNAFUWeapons;@MunghardsItempack;@VanillaPlusPlusMap;@AirdropUpgraded;@TraderPlus; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
+DayZServer_x64.exe -cpuCount=4 -mod=@GameLabs;@CF;@CommunityOnlineTools;@DabsFramework;@Chiemsee;@SkyZ;@DayZEditorLoader;@AdditionalMedicSupplies;@TeddysWeaponPack;@RUSFormavehicles;@StreetLighting;@Notifications;@Notes;@MunghardsItempack;@BaseBuildingPlus;@BuildAnywherev3;@MuchStuffPack;@BuilderItems;@CannabisPlus;@CarCover;@CodeLock;@EarPlugs;@InventoryInCar;@ZeroyFishingZ;@zSpawnSelection;@RaGHummerArmored;@PVEZ;@SNAFUWeapons;@MunghardsItempack;@VanillaPlusPlusMap;@AirdropUpgraded;@TraderPlus; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
 timeout 10
 goto START
