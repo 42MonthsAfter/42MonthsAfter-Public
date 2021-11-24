d0e99af10304c7aa440b356474c9f4289eeb0bf6
42MonthsAfter <rylickliter@gmail.com>
2021-11-24 00:59:30 -0500

    Adding King of the Hill

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 :START
-DayZServer_x64.exe -cpuCount=4 -mod=@GameLabs;@CF;@CommunityOnlineTools;@DabsFramework;@Chiemsee;@SkyZ;@DayZEditorLoader;@ModularVestSystem;@AdditionalMedicSupplies;@TeddysWeaponPack;@MunghardsItempack;@RUSFormavehicles;@StreetLighting;@Notifications;@Notes;@BaseBuildingPlus;@BuildAnywherev3;@MuchStuffPack;@BuilderItems;@CannabisPlus;@CarCover;@CodeLock;@EarPlugs;@InventoryInCar;@ZeroyFishingZ;@zSpawnSelection;@RaGHummerArmored;@PVEZ;@SNAFUWeapons;@VanillaPlusPlusMap;@AirdropUpgraded;@AirRaid;@TraderPlus; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
+DayZServer_x64.exe -cpuCount=4 -mod=@GameLabs;@CF;@CommunityOnlineTools;@DabsFramework;@Chiemsee;@SkyZ;@DayZEditorLoader;@ModularVestSystem;@AdditionalMedicSupplies;@TeddysWeaponPack;@MunghardsItempack;@RUSFormavehicles;@StreetLighting;@KingoftheHill;@Notifications;@Notes;@BaseBuildingPlus;@BuildAnywherev3;@MuchStuffPack;@BuilderItems;@CannabisPlus;@CarCover;@CodeLock;@EarPlugs;@InventoryInCar;@ZeroyFishingZ;@zSpawnSelection;@RaGHummerArmored;@PVEZ;@SNAFUWeapons;@VanillaPlusPlusMap;@AirdropUpgraded;@AirRaid;@TraderPlus; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
 timeout 10
 goto START
