2950a9b8866056be2decc6d92752b36e4a40a91d
Ry Lickliter <rylickliter@gmail.com>
2021-11-25 20:40:32 -0500

    Adding MVS to 42MA Mod
    Updated new gear for faction members with larger load out capacities

---
+++ @42MonthsAfter/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2395829441;
+name = "42 Months After";
+timestamp = 5249420887234850397;
--- @ModularVestSystem/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 1962144102;
-name = "Modular Vest System";
-timestamp = 5249317701000794440;
