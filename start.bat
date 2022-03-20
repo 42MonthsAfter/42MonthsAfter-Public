4b6dbb5f39c070978f0f419b4b853f00c62ed950
Bunker <rylickliter@gmail.com>
2022-03-20 10:42:27 -0400

    Server Sync

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 :START
-DayZServer_x64.exe -cpuCount=2 -mod=@GameLabs;@DeerIsle;@CF;@CommunityOnlineTools;@AirdropUpgraded;@DabsFramework;@CodeLock;@BaseBuildingPlus;@BuilderItems;@zSpawnSelection;@BuildAnywherev3;@Notifications;@Notes;@CarCover;@DayZEditorLoader;@FlipTransport;@InventoryInCar;@VanillaPlusPlusMap;@EarPlugs;@eAI;@42MonthsAfter;@ZeroyFishingZ;@SNAFUWeapons;@CannabisPlus;@TraderPlus; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
+DayZServer_x64.exe -cpuCount=2 -mod=@DeerIsle;@CF;@CommunityOnlineTools;@AirdropUpgraded;@DabsFramework;@CodeLock;@BaseBuildingPlus;@BuilderItems;@zSpawnSelection;@BuildAnywherev3;@Notifications;@Notes;@CarCover;@DayZEditorLoader;@FlipTransport;@InventoryInCar;@VanillaPlusPlusMap;@EarPlugs;@eAI;@42MonthsAfter;@ZeroyFishingZ;@SNAFUWeapons;@CannabisPlus;@TraderPlus; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
 timeout 10
 goto START
