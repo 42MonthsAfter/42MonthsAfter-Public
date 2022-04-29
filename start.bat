39075f0e32b930559dbfe86a2dd3964386dd9395
Bunker <rylickliter@gmail.com>
2022-04-29 10:52:48 -0400

    Adding Deer Isle Mod + GameLabs

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 :START
-DayZServer_x64.exe -cpuCount=2 -mod=@DeerIsle; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
+DayZServer_x64.exe -cpuCount=2 -mod=@DeerIsle;@GameLabs; -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=myprofile -scrAllowFileWrite -adminlog
 timeout 10
 goto START
